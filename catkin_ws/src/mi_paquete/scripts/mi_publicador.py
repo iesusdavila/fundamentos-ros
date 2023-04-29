de36def64facfed51a425d3de08c1e1bceea3835
irda <irdavila@espol.edu.ec>
2023-04-29 15:50:16 -0500

    publicador creado

---
+++ catkin_ws/src/mi_paquete/scripts/mi_publicador.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Int32 
+
+if __name__ == '__main__':
+    rospy.init_node('topic_publicador')
+    rospy.loginfo('Nodo inicializado')
+
+    pub = rospy.Publisher('contador', Int32, queue_size=10)
+
+    rate = rospy.Rate(2)
+    count = 0
+
+    while not rospy.is_shutdown():
+        pub.publish(count)
+        rospy.loginfo(count)
+        count += 1
+        rate.sleep()
