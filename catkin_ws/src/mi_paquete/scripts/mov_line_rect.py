0de4cd0323bdad4a60fc989cf3432b025e31c929
irda <irdavila@espol.edu.ec>
2023-06-11 14:19:42 -0500

    Mov: Rotacion and go to Goal

--- catkin_ws/src/mi_paquete/scripts/mov_line_rect.py
+++ catkin_ws/src/mi_paquete/scripts/mov_line_rect.py
@@ -7,11 +7,6 @@ import math
 import time
 from std_srvs.srv import Empty
 
-x=0
-y=0
-z=0
-yaw=0
-
 def poseCallback(pose_message):
     global x
     global y, yaw
@@ -19,6 +14,7 @@ def poseCallback(pose_message):
     y= pose_message.y
     yaw = pose_message.theta
 
+# publicador, velocidad, disntancia en el eje x, va hacia delante?
 def move(velocity_publisher,speed, distance, is_forward):
     velocity_message = Twist()
     
@@ -52,7 +48,98 @@ def move(velocity_publisher,speed, distance, is_forward):
     # detener el robot una vez completada la trayectoria
     velocity_message.linear.x =0
     velocity_publisher.publish(velocity_message)
+
+# velocidad angular, angulo de rotacion, va en sentido horario?
+def rotate (angular_speed_degree, relative_angle_degree, clockwise):
     
+    global yaw
+    velocity_message = Twist()
+    velocity_message.linear.x=0
+    velocity_message.linear.y=0
+    velocity_message.linear.z=0
+    velocity_message.angular.x=0
+    velocity_message.angular.y=0
+    velocity_message.angular.z=0
+
+    # obtener la velocidad actual
+    theta0=yaw
+
+    # transformar de grados a radianes
+    angular_speed=math.radians(abs(angular_speed_degree))
+    
+    # validar si el giro es en sentido horario
+    if (clockwise):
+        velocity_message.angular.z =-abs(angular_speed)
+    else:
+        velocity_message.angular.z =abs(angular_speed)
+
+    # movimiento del angulo empieza en 0
+    angle_moved = 0.0
+    loop_rate = rospy.Rate(10) 
+
+
+    cmd_vel_topic='/turtle1/cmd_vel'
+    velocity_publisher = rospy.Publisher(cmd_vel_topic, Twist, queue_size=10)
+
+    t0 = rospy.Time.now().to_sec()
+
+    while True :
+        rospy.loginfo("Turtlesim rotates")
+        velocity_publisher.publish(velocity_message)
+
+        # tiempo actual en segundos
+        t1 = rospy.Time.now().to_sec()
+
+        # formula de movimiento angular theta=deltaT*velocidadAngular
+        current_angle_degree = (t1-t0)*angular_speed_degree
+        loop_rate.sleep()
+
+        # validar si alzamos la distancia angular establecida            
+        if  (current_angle_degree>relative_angle_degree):
+            rospy.loginfo("Rotacion alcanzada")
+            break
+
+    # cuando termine el giro paramos al robot
+    velocity_message.angular.z =0
+    velocity_publisher.publish(velocity_message)
+
+def go_to_goal(x_goal, y_goal):
+    global x
+    global y, yaw
+
+    velocity_message = Twist()
+    cmd_vel_topic='/turtle1/cmd_vel'
+
+    while (True):
+        # ------------- VELOCIDAD LINEAL -------------
+        # factor k lineal
+        K_linear = 0.5 
+        # calculo de la distancia usando la posicion de inicio y la posicion de fin
+        distance = abs(math.sqrt(((x_goal-x) ** 2) + ((y_goal-y) ** 2)))
+
+        # velocidad lineal final
+        linear_speed = distance * K_linear
+
+        # ------------- VELOCIDAD ANGULAR -------------
+        # factor k angular
+        K_angular = 4.0
+        # calculo del angulo de rotacion usando la posicion de inicio y la posicion de fin
+        desired_angle_goal = math.atan2(y_goal-y, x_goal-x)
+        
+        # velocidad angular final
+        angular_speed = (desired_angle_goal-yaw)*K_angular
+
+        # ------------- ASIGNAR LAS VELOCIDADES -------------
+        velocity_message.linear.x = linear_speed
+        velocity_message.angular.z = angular_speed
+        
+        velocity_publisher.publish(velocity_message)
+        
+        print('x=', x, 'y=',y)
+
+        if (distance <0.01):
+            print('Posicion final alcanzada')
+            break
 
 if __name__ == '__main__':
     try:
@@ -67,8 +154,10 @@ if __name__ == '__main__':
 
  
         time.sleep(2)
-        print ('Movimiento en lìnea recta: ')
-        move (velocity_publisher,1.0, 3.0, is_forward=True)
+        #move (velocity_publisher,1.0, 3.0, is_forward=True)
+        #rotate (30, 45, clockwise=False)
+        go_to_goal(11.5, 8)
+
         #time.sleep(2)
         #print ('Inicia reset: ')
         #rospy.wait_for_service('reset')
